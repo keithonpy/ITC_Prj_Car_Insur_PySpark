09120cf12cc7192ab021347821b8f0dd4401a65b
Keith Tang <pokpok9301@gmail.com>
2024-03-12 08:55:20 +0000

    update main file to add incremental load

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from os.path import abspath
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import regexp_replace, col
 from FullLoad import FullLoad
+from IncrLoad import IncrLoad
 
 
 
 if __name__ == "__main__":
-    # PostgresSQL connection properties
+     # PostgresSQL data and Hive data
     postgres_url = "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb"
     postgres_properties = {
         "user": "consultants",
@@ -19,4 +20,18 @@ if __name__ == "__main__":
         "hive_database_name": "project1db",
         "hive_table_name": "carInsuranceClaims"
     }
+
+
+
+    print("###############################################")
+    print("############## Full Load ######################")
+    print("###############################################")
+   
     FullLoad(postgres_url, postgres_properties, postgres_table_name, hive_data)
+
+    print("###############################################")
+    print("######### Incremental Load ####################")
+    print("###############################################")
+
+
+    IncrLoad(postgres_url, postgres_properties, postgres_table_name, hive_data)
